e2e1adb6e215fac171090235566fc485bdd45bb8
akane-senseki <ajajggpw@gmail.com>
2021-04-12 19:40:40 +0900

    add Entity/Product.php

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM ;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="product")
+ */
+
+class Product{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id" , type="integer")
+     * ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id ;
+
+    /**
+     * @ORM\Column(name="name" , type="string")
+     */
+    protected $name ;
+    
+    public function getId(){
+        return $this->id ;
+    }
+
+    public function getName(){
+        return $this->name ;
+    }
+
+    public function setName($name){
+        $this->name = $name ;
+    }
+}
