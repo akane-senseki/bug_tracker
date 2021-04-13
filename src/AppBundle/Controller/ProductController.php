b4239c7393bffc137386981a2289e8deb5e6ab60
akane-senseki <ajajggpw@gmail.com>
2021-04-13 13:27:49 +0900

    show bugs count

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -26,8 +26,15 @@ class ProductController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $products = $em->getRepository('AppBundle:Product')->findAll();
+        $dql = "SELECT p.id , p.name , count(b.id) AS openBugs FROM AppBundle:Bug b " .
+               "JOIN b.products p WHERE b.status = 'OPEN' GROUP BY p.id" ;
+        echo "$dql";
 
+        $products = $em->createQuery($dql)->getScalarResult();
+        //以下の書き方はgetScalarResult()と同じ
+        //$products = $em->createQuery($dql)->setHydrationMode(Query::HYDRATE_SCALAR)->getResult();
+
+        dump($products);
         return $this->render('product/index.html.twig', array(
             'products' => $products,
         ));
